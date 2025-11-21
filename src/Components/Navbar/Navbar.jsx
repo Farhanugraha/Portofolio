ab5e8908393d7dd1e62016af430c69c2c557b092
Farhan Nugraha <farhanoegrahaa@gmail.com>
2025-11-21 16:40:40 +0700

    first commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/navlogo.svg";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        <ul className="nav-menu">
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Services</li>
-            <li>Contact</li>
-        </ul>
-        <div className="nav-connect">Connect With Me</div>
+    <div className="navbar">
+      {/* <img src={logo} alt="Logo" className="nav-logo" /> */}
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect With Me</div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
